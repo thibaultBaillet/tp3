a35cff0240b1bc862e9eeed258fe71dfb17023ca
thibaultBaillet <79105144+thibaultBaillet@users.noreply.github.com>
2021-03-08 12:31:30 +0100

    Add files via upload

--- question2/Number.java
+++ question2/Number.java
@@ -50,37 +50,4 @@ public class Number extends Observable{
 
         }else {this.value = VALEUR_MIN; }
     }
-
-
-
-/*    public void addValueListener(ValueListener listener){
-        listeners.add(ValueListener.class, listener);
-    }
-    public void removeVolumeListener(ValueListener l){
-        listeners.remove(ValueListener.class, l);
-    }
-    public void ValueChanged(){
-        ValueListener[] listenerList = (ValueListener[])listeners.getListeners(ValueListener.class);
-
-        for(ValueListener listener : listenerList){
-            listener.valueChanged(new ValueChangedEvent(this, getValue()));
-        }
-}*/
-/*
-public interface  ValueListener extends EventListener{
-    public void valueChanged(ValueChangedEvent event);
-}
-
-public class ValueChangedEvent extends EventObject{
-    private int newValue;
-
-    public ValueChangedEvent(Object source, int newVolume){
-        super(source);
-
-        this.newValue = newVolume;
-    }
-
-    public int getNewValue(){
-        return newValue;
-    }*/
 }
